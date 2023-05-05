bb23c76dd0ba06c20541f4da3c45498343527c4b
tddomingues <129981877+tddomingues@users.noreply.github.com>
2023-05-05 16:14:31 -0300

    Create codigo.js

---
+++ carrosel/codigo.js
@@ -0,0 +1,31 @@
+import { LightningElement } from 'lwc';
+
+export default class Carousel extends LightningElement {
+  currentSlide = 1;
+
+  //connectedCallback() {
+  //  // Exibe a primeira imagem imediatamente
+  //  this.showSlide(this.currentSlide);
+  //}
+
+  handleNextClick() {
+    this.currentSlide = this.currentSlide === 3 ? 1 : this.currentSlide + 1;
+    this.showSlide(this.currentSlide);
+  }
+
+  handlePrevClick() {
+    this.currentSlide = this.currentSlide === 1 ? 3 : this.currentSlide - 1;
+    this.showSlide(this.currentSlide);
+  }
+
+  showSlide(slideNumber) {
+    const slides = this.template.querySelectorAll('.carroselImg');
+    slides.forEach((slide, index) => {
+      if (index === slideNumber - 1) {
+        slide.classList.add('acao');
+      } else {
+        slide.classList.remove('acao');
+      }
+    });
+  }
+}
